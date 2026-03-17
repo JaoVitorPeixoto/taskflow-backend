09fd131bfe87bd751b12a3380ac1a0d2a73f89fd
João Peixoto - Personal Notebook <joaottvitor04@gmail.com>
2026-03-17 16:04:30 -0300

    feat: Adiciona implementação de IUserRepository

---
+++ src/TaskFlow.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,60 @@
+using System.Linq.Expressions;
+
+using Microsoft.EntityFrameworkCore;
+
+using TaskFlow.Domain.Abstractions;
+using TaskFlow.Domain.Entities;
+using TaskFlow.Domain.ValueObjects;
+
+using Task = System.Threading.Tasks.Task;
+
+namespace TaskFlow.Infrastructure.Persistence.Repositories;
+
+public class UserRepository : IUserRepository
+{
+
+    private readonly AppDbContext _context;
+
+    public UserRepository(AppDbContext context)
+    {
+        _context = context;   
+    }
+
+    public async Task AddAsync(User entity, CancellationToken cancellationToken =default)
+    {
+       await  _context.Users.AddAsync(entity, cancellationToken);
+    }
+
+    public Task DeleteAsync(User entity)
+    {
+        _context.Users.Remove(entity);
+        return Task.CompletedTask;
+    }
+
+    public async Task<bool> EmailExistsAsync(Email email, CancellationToken cancellationToken = default)
+    {
+        return await _context.Users.AnyAsync(u => u.Email == email, cancellationToken);
+    }
+
+    public async Task<IEnumerable<User>> GetAllAsync(Expression<Func<User, bool>>? filter, CancellationToken cancellationToken = default)
+    {
+        var query = filter != null ? _context.Users.Where(filter) : _context.Users;
+        return await query.ToListAsync(cancellationToken);
+    }
+
+    public async Task<User?> GetByEmailAsync(Email email, CancellationToken cancellationToken = default)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.Email == email, cancellationToken);
+    }
+
+    public async Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.Id == id, cancellationToken);
+    }
+
+    public Task UpdateAsync(User entity)
+    {
+        _context.Users.Update(entity);
+        return Task.CompletedTask;
+    }
+}
