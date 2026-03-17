1576a6887c2e7a31ed91def863cebf5fed3c64ca
João Peixoto - Personal Notebook <joaottvitor04@gmail.com>
2026-03-17 15:51:47 -0300

    refactor: Adiciona o Cancellation Token nos métodos que restavam.

--- src/TaskFlow.Domain/Abstractions/IRepositoryBase.cs
+++ src/TaskFlow.Domain/Abstractions/IRepositoryBase.cs
@@ -9,7 +9,7 @@ public interface IRepositoryBase<T> where T : EntityBase
 {
     Task<T> GetByIdAsync(Guid id, CancellationToken cancellationToken =default);
     Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter, CancellationToken cancellationToken = default);
-    Task AddAsync(T entity);
+    Task AddAsync(T entity, CancellationToken cancellationToken = default);
     Task UpdateAsync(T entity);
     Task DeleteAsync(T entity);
 }
